8a553e188e7f4714531ee87da61642056006e4f9
Stanisław Griszko <s.griszko@gmail.com>
2023-03-17 13:34:35 +0100

    refactor: clean db.js code

--- db.js
+++ db.js
@@ -1,13 +1,2 @@
-// const sqlite3 = require('sqlite3').verbose();
 import sqlite3 from 'sqlite3';
 export const db = new sqlite3.Database('database.db');
-
-// db.run(
-//   'CREATE TABLE user (id INTEGER PRIMARY KEY, username TEXT, password TEXT)'
-// );
-
-// const password = 'updated2';
-// const user_id = 'Boby';
-
-// db.run('UPDATE users SET hash=? WHERE username=?', [password, user_id]);
-// db.run('DROP TABLE user');
