7bf3343a466b58124b84732daa412ac46b7de309
Stanisław Griszko <s.griszko@gmail.com>
2023-03-17 14:48:22 +0100

    refactor: delete form tag from account

--- controllers/favSetups.js
+++ controllers/favSetups.js
@@ -5,14 +5,14 @@ export const favSetups = async (req, res) => {
   const { model, location, setup_id } = req.params;
 
   try {
-    const car_id = await new Promise((resolve, reject) => {
+    const carId = await new Promise((resolve, reject) => {
       db.get('SELECT id FROM cars WHERE model=?', [model], (err, row) => {
         if (err) reject(err);
         resolve(row.id);
       });
     });
 
-    const location_id = await new Promise((resolve, reject) => {
+    const locationId = await new Promise((resolve, reject) => {
       db.get(
         'SELECT id FROM locations WHERE location_name=?',
         [location],
@@ -23,25 +23,21 @@ export const favSetups = async (req, res) => {
       );
     });
 
-    // const locationId = location_id[0].id;
-
     const car = await new Promise((resolve, reject) => {
       db.get(
         'SELECT brand, model, class FROM cars INNER JOIN setups ON cars.id=setups.cars_id WHERE setups.cars_id IN (SELECT id FROM cars WHERE id=?)',
-        [car_id],
+        [carId],
         (err, rows) => {
           if (err) reject(err);
           resolve(rows);
         }
       );
     });
-
-    console.log(car);
 
     const locations = await new Promise((resolve, reject) => {
       db.get(
         'SELECT location_name FROM locations INNER JOIN setups ON locations.id=setups.locations_id WHERE setups.locations_id IN (SELECT id FROM locations WHERE id=?)',
-        [location_id],
+        [locationId],
         (err, rows) => {
           if (err) reject(err);
           resolve(rows);
@@ -49,7 +45,7 @@ export const favSetups = async (req, res) => {
       );
     });
 
-    const car_setups = await new Promise((resolve, reject) => {
+    const carSetups = await new Promise((resolve, reject) => {
       db.all('SELECT * FROM setups WHERE id=?', [setup_id], (err, rows) => {
         if (err) reject(err);
         resolve(rows);
@@ -62,7 +58,7 @@ export const favSetups = async (req, res) => {
       car: car,
       locations: locations,
       model: model,
-      setups: car_setups,
+      setups: carSetups,
     });
   } catch (err) {
     console.error(err);
